e7a4a78152caf8dc81dba24c20f55fa17dff3e31
AbnerValdiviaD <alejandro.valdivia@gluo.mx>
2026-02-19 16:14:52 -0600

    fix: fix tests to connect validation

--- processor/jest.config.ts
+++ processor/jest.config.ts
@@ -4,11 +4,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['./test'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.m?js$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(msw|@bundled-es-modules|@mswjs|until-async|strict-event-emitter)/)',
-  ],
 };
